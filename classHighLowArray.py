f04460c99bbb1566dc3c7241b4eca95389c40e92
jvaler1 <51203862+jvaler1@users.noreply.github.com>
2025-03-26 20:08:14 -0400

    Add files via upload

---
+++ classHighLowArray.py
@@ -0,0 +1,36 @@
+# Create a variable to hold the highest value
+# Assign the value at element 0 to the highest
+# Use a looop to step through the rest of the elements
+# Each iteration a comparison is made to the highest variable
+# If the element is greater than the highest value, that value is then assigned to the highest variable
+
+import random
+
+def getRandomList(start, end, length):
+    randomList = []
+    while length > 0:
+        length -= 1
+        randomList.append(random.randint(start, end))
+    return randomList
+
+def listHighest(localList):
+    highest = localList[0]
+    for x in localList:
+        if x > highest:
+            highest = x
+    print(f"Highest: {highest}")
+
+def listLowest(localList):
+    lowest = localList[0]
+    for x in localList:
+        if x < lowest:
+            lowest = x
+    print(f"Lowest: {lowest}")
+
+def main():
+    numList = getRandomList(-100, 100, 10)
+    print(numList)
+    listHighest(numList)
+    listLowest(numList)
+
+main()
